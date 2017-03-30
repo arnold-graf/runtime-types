5abd6e704879308afef8d5efacef2ac4096b5494
Arnold Graf <arnold.graf@gmail.com>
2017-03-30 20:54:11 +0200

    experiments with nested objects and recursion

--- src/validate.js
+++ src/validate.js
@@ -5,6 +5,7 @@
 import type {Type, Property, ObjectMap} from './types'
 import {extend} from 'lodash'
 
+import * as _ from 'lodash'
 export type Validator<T> = (value:T) => ValidationResult
 export type ValidationError = string;
 // either true, or a string with the error
@@ -29,8 +30,11 @@ type KeyedValidator = [string, Validator<any>];
 // create a single validator: a function to call with your object
 // it will return an array of errors
 
-export function create(map:ValidatorMap, type:Type):ValidateObject {
-  var vs = typeValidators(map, type)
+export function create(map:ValidatorMap, type:Type, contextTypes:ObjectMap<Type>):ValidateObject {
+  var vs = typeValidators(map, type, contextTypes)
+  console.log(require('util').inspect(type, { depth: null }))
+  console.log('vs:')
+  console.log(require('util').inspect(vs, { depth: null }))
   return function(obj) {
     return validateAll(vs, obj)
   }
@@ -39,11 +43,12 @@ export function create(map:ValidatorMap, type:Type):ValidateObject {
 export function createAll(map:ValidatorMap, types:ObjectMap<Type>):ObjectMap<ValidateObject> {
   var vs = {}
   for (var name in types) {
-    vs[name] = create(map, types[name])
+    console.log('-------');
+    console.log('name',name);
+    vs[name] = create(map, types[name], types)
   }
   return vs
 }
-
 
 // ---------------------------------------------------------------
 
@@ -84,76 +89,88 @@ function valid(result:ValidationResult):boolean {
 
 // turns a property into a validator
 // ignore optional, it doesn't work right
-function propToValidator(map:ValidatorMap, prop:Property):KeyedValidator {
-  return typeToValidator(map, prop.key, prop.type)
+function propToValidator(map:ValidatorMap, prop:Property, contextTypes:ObjectMap<Type>):KeyedValidator {
+  if (contextTypes[prop.type.name]) {
+    console.log('prop.type.name', prop.type.name)
+    console.log('contextTypes[prop.type.name]', contextTypes[prop.type.name])
+    return typeToValidator(map, prop.key, contextTypes[prop.type.name])
+  }else{
+    return typeToValidator(map, prop.key, prop.type)
+  }
 }
 
 // just do required for now?
 // you want to allow them to override the mapping
 // especially for their custom types!
 function typeToValidator(map:ValidatorMap, key:string, type:Type):KeyedValidator {
-
+  // console.log('key', key)
+  // console.log('type', type)
+  // console.log('map', map)
   // TODO nested objects
-  //if (type.properties) {
+  if (type.properties || type.params) {
 
-    ////console.log("TWE", type.properties)
-    //var all = objToValidators(map, type.properties)
+    console.log("TWE", type.properties)
+    console.log("TWA", type.params);
+    // var all = objToValidators(map, type.properties, contextTypes)
 
-    //function allThemBones(value) {
-      //var errs = validateAll(all, value)
-
-      //if (errs.length) {
-        //return "error there was some stuff: " + errs.length
-      //}
-
-      //return true
-    //}
-
-    //return [key, allThemBones]
-  //}
+    // function allThemBones(value) {
+    //   var errs = validateAll(all, value)
+    //
+    //   if (errs.length) {
+    //     return "error there was some stuff: " + errs.length
+    //   }
+    //
+    //   return true
+    // }
+    //
+    // return [key, allThemBones]
+  }
 
   // now run the type-based validator
   var validator = map[type.name]
-
   if (!validator) {
     throw new Error("Could not find validator for type: " + type.name)
   }
 
   function isValid(value) {
+    console.log('key', key)
+    console.log('value', value)
     if (!exists(value)) {
       // if the property doesn't exist, and it's not a nullable property
       // otherwise just do the second one
       if (type.nullable) {
         return true
-      }
-
-      else {
+      }else {
         return "missing"
       }
-    }
-
-    else {
+    }else {
       return validator(value)
     }
   }
-
   return [key, isValid]
 }
 
-function typeValidators(map:ValidatorMap, type:Type):Array<KeyedValidator> {
+function typeValidators(map:ValidatorMap, type:Type, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
+  // console.log('typeValidators',type);
   var fullMap:ValidatorMap = extend(map, VALIDATORS_BY_TYPE)
   if (type.properties) {
-    return objToValidators(fullMap, type.properties)
-  }
-
-  else {
-    return [typeToValidator(map, "", type)]
+    return objToValidators(fullMap, type.properties, contextTypes)
+  if (type.params) {
+    return arrToValidators(fullMap, type.params, contextTypes)
+  }else {
+    return [typeToValidator(map, "", type, contextTypes)]
   }
 }
 
-function objToValidators(map:ValidatorMap, props:Array<Property>):Array<KeyedValidator> {
+function objToValidators(map:ValidatorMap, props:Array<Property>, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
   return props.map(function(prop) {
-    return propToValidator(map, prop)
+    return propToValidator(map, prop, contextTypes)
+  })
+}
+
+function arrToValidators(map:ValidatorMap, params:Array<Property>, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
+  return params.map((param) => {
+    return paramToValidator(map, param, contextTypes)
   })
 }
 
