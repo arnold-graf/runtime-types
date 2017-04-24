809e4c7a521e1859c5372e01064dc29066aa4da1
Arnold Graf <arnold.graf@gmail.com>
2017-04-24 11:18:24 +0200

    switch validator

--- src/validate.js
+++ src/validate.js
@@ -1,213 +1,183 @@
-// @flow
 
-// TODO FEATURE support nested objects
+var _ = require('lodash')
 
-import type {Type, Property, ObjectMap} from './types'
-import {extend} from 'lodash'
-
-import * as _ from 'lodash'
-export type Validator<T> = (value:T) => ValidationResult
-export type ValidationError = string;
-// either true, or a string with the error
-// use === true to test
-export type ValidationResult = boolean | ValidationError;
-
-export type ValidatorMap = {[key:string]:Validator<any>}
-
-export type ValidateObject = (value:Object) => Array<KeyedError>
-
-export type KeyedError = {
-  key: string;
-  value: string;
-  error: ValidationError;
-}
-
-type KeyedValidator = [string, Validator<any>];
-
-
-// -------------------------------------------------------------
-
-// create a single validator: a function to call with your object
-// it will return an array of errors
-
-export function create(map:ValidatorMap, type:Type, contextTypes:ObjectMap<Type>):ValidateObject {
-  var vs = typeValidators(map, type, contextTypes)
-  console.log(require('util').inspect(type, { depth: null }))
-  console.log('vs:')
-  console.log(require('util').inspect(vs, { depth: null }))
-  return function(obj) {
-    return validateAll(vs, obj)
+var validateTypeOf = (type) => {
+  return (val) => {
+    if (typeof val === type) {
+      return true
+    }else{
+      // TODO: throw
+      return "expected typeof " + type + ", got " + typeof val
+    }
   }
 }
 
-export function createAll(map:ValidatorMap, types:ObjectMap<Type>):ObjectMap<ValidateObject> {
-  var vs = {}
-  for (var name in types) {
-    console.log('-------');
-    console.log('name',name);
-    vs[name] = create(map, types[name], types)
+var validateInstanceOf = (func) => {
+  return (val) => {
+    if (val instanceof func) {
+      return true
+    }else{
+      // TODO: throw
+      return "expected instance of " + func.name
+    }
   }
-  return vs
 }
 
-// ---------------------------------------------------------------
-
-var VALIDATORS_BY_TYPE:ValidatorMap = {
+var validatePrimitives = {
   "string"  : validateTypeOf("string"),
   "number"  : validateTypeOf("number"),
   "boolean" : validateTypeOf("boolean"),
-  "Date"    : validateInstanceOf(Date),
-  "Object"  : validateExists(),
-  "Array"   : validateArray(),
+  "Date"    : validateInstanceOf(Date)
 }
 
-function validateAll(vs:Array<KeyedValidator>, obj:Object):Array<KeyedError> {
-  var maybeErrs:Array<?KeyedError> = vs.map(function(kv) {
-    return validate(kv, obj)
-  })
+var validateObject = (contextTypes, type, value) => {
+  return _.map(type.properties, (prop) => {
 
-  var errs:any = maybeErrs.filter(function(err:?KeyedError):boolean {
-    return (err !== undefined)
-  })
+    // console.log('PROP TYPE');
+    // debug(prop)
+    // console.log('PROP VALUE');
+    // debug(value)
 
-  return errs
-}
-
-function validate([key, validator]:KeyedValidator, obj:Object):?KeyedError {
-  // this runs the validator
-  var result = validator(obj[key])
-  if (!valid(result)) {
-    return {key: key, value: obj[key], error: (result : any)}
-  }
-}
-
-function valid(result:ValidationResult):boolean {
-  return result === true
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-// turns a property into a validator
-// ignore optional, it doesn't work right
-function propToValidator(map:ValidatorMap, prop:Property, contextTypes:ObjectMap<Type>):KeyedValidator {
-  if (contextTypes[prop.type.name]) {
-    console.log('prop.type.name', prop.type.name)
-    console.log('contextTypes[prop.type.name]', contextTypes[prop.type.name])
-    return typeToValidator(map, prop.key, contextTypes[prop.type.name])
-  }else{
-    return typeToValidator(map, prop.key, prop.type)
-  }
-}
-
-// just do required for now?
-// you want to allow them to override the mapping
-// especially for their custom types!
-function typeToValidator(map:ValidatorMap, key:string, type:Type):KeyedValidator {
-  // console.log('key', key)
-  // console.log('type', type)
-  // console.log('map', map)
-  // TODO nested objects
-  if (type.properties || type.params) {
-
-    console.log("TWE", type.properties)
-    console.log("TWA", type.params);
-    // var all = objToValidators(map, type.properties, contextTypes)
-
-    // function allThemBones(value) {
-    //   var errs = validateAll(all, value)
-    //
-    //   if (errs.length) {
-    //     return "error there was some stuff: " + errs.length
-    //   }
-    //
-    //   return true
-    // }
-    //
-    // return [key, allThemBones]
-  }
-
-  // now run the type-based validator
-  var validator = map[type.name]
-  if (!validator) {
-    throw new Error("Could not find validator for type: " + type.name)
-  }
-
-  function isValid(value) {
-    console.log('key', key)
-    console.log('value', value)
-    if (!exists(value)) {
-      // if the property doesn't exist, and it's not a nullable property
-      // otherwise just do the second one
-      if (type.nullable) {
-        return true
-      }else {
-        return "missing"
-      }
-    }else {
-      return validator(value)
+    // it’s optional and missing => ok
+    if (prop.optional === true && isUndefined(value[prop.key])) {
+      return true
+    // it’s not optional and missing => error
+    }else if (!prop.optional && isUndefined(value[prop.key])){
+      throw new TypeError(`Property ”${prop.key}”: expected a value, got missing (${value[prop.key]})`)
+    // else => validate
+    }else{
+      return { [prop.key]: validate(contextTypes, prop.type.name, value[prop.key], prop.type) }
     }
-  }
-  return [key, isValid]
-}
-
-function typeValidators(map:ValidatorMap, type:Type, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
-  // console.log('typeValidators',type);
-  var fullMap:ValidatorMap = extend(map, VALIDATORS_BY_TYPE)
-  if (type.properties) {
-    return objToValidators(fullMap, type.properties, contextTypes)
-  if (type.params) {
-    return arrToValidators(fullMap, type.params, contextTypes)
-  }else {
-    return [typeToValidator(map, "", type, contextTypes)]
-  }
-}
-
-function objToValidators(map:ValidatorMap, props:Array<Property>, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
-  return props.map(function(prop) {
-    return propToValidator(map, prop, contextTypes)
   })
 }
 
-function arrToValidators(map:ValidatorMap, params:Array<Property>, contextTypes:ObjectMap<Type>):Array<KeyedValidator> {
-  return params.map((param) => {
-    return paramToValidator(map, param, contextTypes)
-  })
-}
-
-//////////////////////////////////////////////////////////////
-// Validators
-//////////////////////////////////////////////////////////////
-
-export function validateExists():Validator<any> {
-  return function(val) {
-    return exists(val) || "missing"
+var validateArray = (contextTypes, type, value) => {
+  if (Array.isArray(value)) {
+    if (type.params) {
+      return _.map(value, (v) => {
+        return validate(contextTypes, type.params[0].name, v, type.params[0])
+      })
+    }else{
+      return true
+    }
+  }else{
+    throw new TypeError(`Property ”${type.params[0].name}”: expected an array, got (${typeof(value)})`)
   }
 }
 
-export function validateTypeOf(type:string):Validator<any> {
-  return function(val) {
-    return (typeof val === type) || "expected typeof " + type
+var validateUnion = (contextTypes, type, value) => {
+  if (Array.isArray(type.types)) {
+    var isValid = _.some(type.types, (type) => {
+      try {
+        var validation = validate(contextTypes, type.name, value, type)
+        console.log('validation', validation);
+        return validation
+      }catch (e){
+        return false
+      }
+    })
+    if (isValid) {
+      return true
+    }else{
+      throw new TypeError(`Union Type could not be validated.`)
+    }
+  }else{
+    throw new TypeError(`Union Types must consist of a list of types.`)
   }
 }
 
-export function validateArray():Validator<any> {
-  return function(val) {
-    return (Array.isArray(val) || "expected array")
+var validateLiteral = (contextTypes, type, value) => {
+  if (type.literal === value) {
+    return true
+  }else{
+    throw new TypeError(`${value} is literally not ${type.literal}`)
   }
 }
 
-export function validateInstanceOf(func:Function):Validator<any> {
-  return function(val) {
-    return (val instanceof func) || "expected instance of " + func.name
+var isNull = (value) => value === null
+
+var isUndefined = (value) => value === undefined
+
+var isAnyType = (type) => type.name == "any"
+
+var isPrimitiveType = (type) => !!validatePrimitives[type.name]
+
+var isNullableType = (type) => !!type.nullable
+
+var isOptionalType = (type) => !!type.optional
+
+var validate = (contextTypes, typename, value, type) => {
+
+  // FIGURE OUT THE `type`
+
+  // the actual type information was not passed in and its name
+  // could not be found in the context types.
+  if (type == undefined && !contextTypes[typename]){
+    throw new TypeError(`Type name ”${typename}” not found in value.`)
+
+  // the actual type information was not passed in, but only
+  // the type’s name. Get the actual type from the context types.
+  } else if (contextTypes[typename]) {
+    type = contextTypes[typename]
+  }
+  // if (typename) {
+  //   console.log('TYPENAME',typename);
+  // }
+  console.log('TYPE:', type)
+  // debug(type)
+  console.log('VALUE:',value)
+  // debug()
+
+  // EXISTENTIAL CHECKS
+
+  // it’s not nullable yet null: error
+  if (!isNullableType(type) && !isAnyType(type) && isNull(value)) {
+    throw new TypeError(`${type.name} cannot be null.`)
+
+  // it’s nullable and null: ok
+  }else if (isNullableType(type) && isNull(value)) {
+    // console.log('NULLABLE & NULL')
+    return true
+
+  // it’s not optional and undefined: error
+  }else if (!isOptionalType(type) && isUndefined(value)){
+    throw new TypeError(`${type.name} cannot be undefined`)
+
+  // it’s optional and undefined: ok
+  }else if (isOptionalType(type) && isUndefined(value)){
+    // console.log('OPTIONAL & UNDEFINED')
+    return true
+  }
+
+  // PERFORM ACTUAL TYPE CHECKS
+  else{
+    // console.log('NONE OF THE EXISTENTIAL CONDITIONS APPLY')
+    // PRIMITIVES
+    if (isPrimitiveType(type)) {
+      return validatePrimitives[type.name](value)
+    // TYPE ANY
+    }else if (isAnyType(type)){
+      return !isUndefined(value)
+    // LITERAL
+    }else if (type.name.indexOf('Literal') !== -1){
+      return validateLiteral(contextTypes, type, value)
+
+    // ALL OF THESE CALL RECURSIONS
+    // OBJECT
+    }else if (type.name == 'Object'){
+      return validateObject(contextTypes, type, value)
+    // ARRAY
+    }else if (type.name == 'Array'){
+      return validateArray(contextTypes, type, value)
+    // UNION TYPE
+    }else if (type.name == 'Union'){
+      return validateUnion(contextTypes, type, value)
+    // TYPE ALIAS (COMPLEX)
+    }else{
+      return validate(contextTypes, null, value, type)
+    }
+
   }
 }
 
-export function validateRegex(regex:RegExp):Validator<any> {
-  return function(val) {
-    return (regex.test(val)) || "did not match " + regex.toString()
-  }
-}
-
-function exists(value):boolean {
-  return !(value === undefined || value === null)
-}
+module.exports.validate = validate
