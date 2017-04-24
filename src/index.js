ed54d99b839ab2baa9a2b501d3a6186e7804c5a0
Arnold Graf <arnold.graf@gmail.com>
2017-04-24 11:18:16 +0200

    break api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 // @flow
-import * as validate from './validate'
-import type {Property, Type} from './types'
-import {readFile} from './parse'
 
-export {readFile, validate}
+var validate = require('./validate').validate
+import type {Property, Type} from './types'
+import { readFile as getTypes } from './parse'
+
+var parse = (path:string) => {
+  var typeMap = getTypes(path)
+  return {
+    assert : (value:any, typename:string) => {
+      return validate(typeMap, typename, value)
+    }
+  }
+}
+
+export {getTypes, parse, validate}
 export type {Type, Property}
